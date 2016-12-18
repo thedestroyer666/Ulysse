5b594d0d663b7d4ad9f346df94ae61f210426269
eric.mariacher@gmail.com <eric.mariacher@gmail.com>
2016-12-18 17:56:27 +0100

    correction et commentaires

--- jeux_algo_echecs_and_co/main.c
+++ jeux_algo_echecs_and_co/main.c
@@ -37,7 +37,7 @@ int main()
         nbreParticipants=1;
         printf("nombre de participants? \n");
         scanf("%d",&nbreParticipants);
-        if (nbreParticipants<1)
+        if (nbreParticipants<1) //verification nombre de participants
         {
             printf("erreur, retapez le nombre de participants \n");
         }
@@ -46,52 +46,52 @@ int main()
     struct Participants tabParticipants[nbreParticipants]; //le tableau contenant les participants
 //afficher le tableau des jeux (en numérotant les cases)
     int cptJeu; //compteur jeu
-    for (cptJeu=0; cptJeu<NB_JEUX; cptJeu++ )
+    for (cptJeu=0; cptJeu<NB_JEUX; cptJeu++ ) //affichage tableau de jeu
     {
         printf(" Jeu[%d] %s \n",cptJeu, tabJeux[cptJeu].nom);
     }
 
 //remplir le tableau des participants (nom du joueur et numéro du jeu) et affecter un numéro de groupe
     int cptPart; //compteur boucle remplissage tableau
-    int cptParticipants=0;
-    for (cptPart=0; cptPart<nbreParticipants; cptPart++ )
+    int cptParticipants=0; //cptParticipants
+    for (cptPart=0; cptPart<nbreParticipants; cptPart++ ) //lancement boucle
     {
         printf("nom d'un participant "); // demander nom des participants
         scanf("%s",&(tabParticipants[cptPart].nomP)); //lire
         //demander numero jeu, avec verification
         do
         {
-            printf("numero du jeu? \n");
-            scanf("%d",&numero);
+            printf("numero du jeu? \n"); //demander numéro du jeu
+            scanf("%d",&numero); //lire
 
-            if (numero<0 || numero>=NB_JEUX)
+            if (numero<0 || numero>=NB_JEUX) //verification
             {
                 printf("Erreur, Choisir un numero dans la liste \n");
             }
             tabJeux[numero].cptParticipants++; //On ajoute 1 au nombre de participants
 
         }
-        while (numero<0 || numero>=NB_JEUX);
-        strcpy(tabParticipants[cptPart].nomJ, tabJeux[numero].nom);
+        while (numero<0 || numero>=NB_JEUX); //pour chaque jeu
+        strcpy(tabParticipants[cptPart].nomJ, tabJeux[numero].nom); //transposer nom du jeu dans l'autre structure
         if (tabJeux[numero].cptParticipants>tabJeux[numero].nbreMax) //compter le nombre de groupe
         {
-            (tabJeux[numero].cptGroupes)++;
+            (tabJeux[numero].cptGroupes)++; //rajouter un au nombre de groupe
             tabJeux[numero].cptParticipants=1;// reinitialiser nombre de partipants pour prochain groupe
         }
         printf(" %s vous allez dans groupe %d \n",(tabParticipants[cptPart].nomP),(tabJeux[numero].cptGroupes)); //informer sur le groupe de jeu
         tabParticipants[cptPart].groupe = tabJeux[numero].cptGroupes; //affecte le numero du groupe dans la structure Participant (et sa variable adaptée)
     }
-    for (cptPart=0; cptPart<nbreParticipants; cptPart++ ) //informer le participant sur son jeu
+    for (cptPart=0; cptPart<nbreParticipants; cptPart++ ) //pour chaque participant
     {
-        printf(" Participant[%d] %s joue a %s \n \n",cptPart, tabParticipants[cptPart].nomP, tabParticipants[cptPart].nomJ);
+        printf(" Participant[%d] %s joue a %s \n \n",cptPart, tabParticipants[cptPart].nomP, tabParticipants[cptPart].nomJ); //récapituler le jeu attribué
     }
 
 //trier le tableau des participants
 // tri bulle
     int indAsc, indDesc; //indices de parcours ascendant et descendant
     struct Participants tampon; //tampon
-    int tampongroupe;
-    char tampon2;
+
+
 //Tant qu'il reste des éléments à trier faire
     for(indAsc=0; indAsc<nbreParticipants-1; indAsc++)
     {
@@ -127,25 +127,25 @@ int main()
 
     }
 //informer les participants qui font partie d un groupe dans lequel il n y a pas assez de joueurs
-    printf("\n");
+    printf("\n"); //revenir a la ligne
     int cptPart2; //compteur boucle affichage
 
     for (cptPart2=0; cptPart2<=nbreParticipants-1; cptPart2++) //lancement boucle affichage
     {
         if (cptPart2==0 || strcmp(tabParticipants[cptPart2].nomJ,tabParticipants[cptPart2-1].nomJ)!=0) //afficher une seule fois le nom du jeu, afficher les groupes et les noms des jouerus
         {
-            if (cptPart2==0 || (tabParticipants[cptPart2].groupe!=tabParticipants[cptPart2-1].groupe))
+            if ((cptPart2==0 || (tabParticipants[cptPart2].groupe!=tabParticipants[cptPart2-1].groupe)) || (tabParticipants[cptPart2].nomJ!=tabParticipants[cptPart2-1].nomJ) )
             {
-                printf("%s Groupe %d \nJoueurs\n%s\n", tabParticipants[cptPart2].nomJ,tabParticipants[cptPart2].groupe, tabParticipants[cptPart2].nomP);
+                printf("%s Groupe %d \nJoueurs\n  %s\n", tabParticipants[cptPart2].nomJ,tabParticipants[cptPart2].groupe, tabParticipants[cptPart2].nomP);
             }
-            else printf("Joueurs\n%s\n", tabParticipants[cptPart2].nomP ) ;
+            else printf("Joueurs\n  %s\n", tabParticipants[cptPart2].nomP ) ;
 
         }
         else if (cptPart2==0 || tabParticipants[cptPart2].groupe!=tabParticipants[cptPart2-1].groupe)
         {
-            printf("     Groupe %d  \nJoueurs\n%s \n", tabParticipants[cptPart2].groupe, tabParticipants[cptPart2].nomP);
+            printf("%s Groupe %d  \nJoueurs\n  %s \n", tabParticipants[cptPart2].nomJ,tabParticipants[cptPart2].groupe, tabParticipants[cptPart2].nomP);
         }
-        else printf("%s\n", tabParticipants[cptPart2].nomP);
+        else printf("  %s\n", tabParticipants[cptPart2].nomP);
     }
     numero=0;
     for (numero=0; numero<=NB_JEUX-1; numero++)
@@ -153,7 +153,7 @@ int main()
 
         if ((tabJeux[numero].cptParticipants < tabJeux[numero].nbreMin) && (tabJeux[numero].cptParticipants != 0))
         {
-            printf("Le groupe %d du jeu %s ne contient pas assez de participants, revenez une prochaine fois!", tabJeux[numero].cptGroupes, tabJeux[numero].nom);
+            printf("Le groupe %d du jeu %s ne contient pas assez de participants, revenez une prochaine fois!\n", tabJeux[numero].cptGroupes, tabJeux[numero].nom);
         }
     }
 }
